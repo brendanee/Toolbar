a1bcb6f06397551b74c04ff7e4f4e236fb2a041b
brendanee <brendanee314@gmail.com>
2023-05-21 20:14:35 -0700

    Code Re-import & CSS Changes

--- main.js
+++ main.js
@@ -33,4 +33,53 @@ function addToolbar() {
 }
 // Add toolbar when content loads
 document.addEventListener("DOMContentLoaded", addToolbar, false);
+// Get .json search bar data from Github
+async function getData(url) {
+    // Try to fetch the url
+    try {
+        return await fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            // Set search to receieved data, cannot use return here becuase it has a special purpose
+            search = data;
+            return;
+        })
+    } catch (error) {
+        // On error, just print the error in the console
+        console.error(`Fetch error: ${error}`);
+        return;
+    }
+}
+// Call the get data
+getData("https://raw.githubusercontent.com/brendanee/Taskbar/main/result.json");
+// Listen for any keyup (attached to document). keyup so it doesn't annoy the search bar, which focuses immediately
+document.addEventListener("keyup", function(event) {
+    // If the key pressed are W and last was Q, or vice versa
+    if (lastKeyPressed === "KeyW" && event.code === "KeyQ" || lastKeyPressed === "KeyQ" && event.code === "KeyW") {
+        if (document.querySelector("#b-wrapper").className === "") {
+            showMe();
+            document.getElementById("b-search").value = "";
+        } else {
+            hideMe();
+        }
+        lastKeyPressed = "";
+    } else {
+        // This is in the else so it doesn't trigger twice when QW is pressed
+        lastKeyPressed = event.code;
+    }
+});
+// Called on QW press
+function showMe() {
+    // Show toolbar and focus search bar
+    document.querySelector("#b-wrapper").className = "show";
+    document.getElementById("b-search").focus();
+}
+// Also called on QW press
+function hideMe() {
+    // Hide toolbar and clear search bar
+    document.querySelector("#b-wrapper").className = "";
+    document.getElementById("b-search").value = "";
+}
 }
